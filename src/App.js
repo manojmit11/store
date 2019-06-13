510be984fd83dbf546aa9e4a727e2503d651a2de
Venkata Sai Manoj Pasupuleti <venkatasaimanoj.pasupuleti@hyatt.com>
2019-06-13 06:53:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div>
+         <>
+             <Navbar/>
+             <Switch>
+                 <Route exact path='/' component={ProductList}></Route>
+                 <Route path='/details' component={Details}></Route>
+                 <Route path='/cart' component={Cart}></Route>
+                 <Route component={Default}></Route>
+             </Switch>
+         </>
+     </div>
   );
 }
 
